917d79cddb56714bbf2838839804a3be60e1efdc
Mark Kelly <markkelly@Marks-MacBook-Air-2.local>
2025-06-12 08:28:32 +1000

    Assure logo size

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
-import '../styles/lineicons.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
